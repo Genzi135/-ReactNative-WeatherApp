c0cf558b97464f488a61680da138e825b96cad08
Genzi135 <phu13052002@gmail.com>
2023-12-03 13:36:12 +0700

    complet Weather Screen

--- src/screen/WeatherScreen.jsx
+++ src/screen/WeatherScreen.jsx
@@ -16,30 +16,76 @@ function WeatherScreen({ route }) {
 
     return (
         <View style={styles.container}>
-            <View>
-                <Text>
+            <View style={styles.box}>
+                <Text style={styles.title}>
                     {data.forecast.daily.weather_description}
                 </Text>
-                <Icon name={data.forecast.daily.icon} size={80} color={colors.textColor} />
-                <Text>
+                <Icon name={data.forecast.daily.icon} size={150} color={colors.textColor} />
+                <Text style={styles.locationText}>
                     {data.city} - {data.country}
                 </Text>
-
-
             </View>
-            <ScrollView
-                horizontal
-                showsHorizontalScrollIndicator={false}>
-                <FlatList
+            <View style={styles.box}>
+                <View style={styles.row}>
+                    <View style={styles.card}>
+                        <Text style={styles.text}>
+                            {data.forecast.daily.temperature_min} - {data.forecast.daily.temperature_max}
+                        </Text>
+                        <Icon name='temperature-celsius' size={30} color={colors.textColor} />
+                    </View>
+                    <View style={styles.card}>
+                        <Text style={styles.text}>
+                            {data.forecast.daily.wind_speed} km/h
+                        </Text>
+                        <Icon name='weather-windy' size={30} color={colors.textColor} />
+                    </View>
+                    <View style={styles.card}>
+                        <Text style={styles.text}>
+                            {data.forecast.daily.rain_probability} %
+                        </Text>
+                        <Icon name='weather-pouring' size={30} color={colors.textColor} />
+                    </View>
+                    <View style={styles.card}>
+                        <Text style={styles.text}>
+                            {data.forecast.daily.uv_index}
+                        </Text>
+                        <Text style={{ color: colors.textColor, fontSize: 22, fontWeight: 'bold' }}>UV</Text>
+                    </View>
+                </View>
+            </View>
+            <View style={styles.scrollView}>
+                <View style={styles.row}>
+                    <Icon name="clock-outline" size={25} color={colors.textColor} />
+                    <Text style={styles.text}>Hourly</Text>
+                </View>
+                <ScrollView
                     horizontal
-                    data={data}
-                    renderItem={({ item }) => (
-                        <View>
-
-                        </View>
-                    )}
-                />
-            </ScrollView>
+                    showsHorizontalScrollIndicator={false}>
+                    <FlatList
+                        horizontal
+                        data={data.forecast.hourly}
+                        renderItem={({ item }) => (
+                            <View style={styles.hourlyCard}>
+                                <Text style={styles.textCard}>
+                                    {item.time}
+                                </Text>
+                                <View style={styles.row}>
+                                    <Text style={styles.textCard}>
+                                        {item.temperature}
+                                    </Text>
+                                    <Icon name="temperature-celsius" size={18} color={colors.textColor} />
+                                </View>
+                                <Icon name={item.icon} size={40} color={colors.textColor} />
+                                <Text style={styles.textCard}>
+                                    {item.cloudiness}
+                                </Text>
+                                <Text style={styles.textCard}>Humidity: {item.humidity}%</Text>
+                                <Text style={styles.textCard}>Rain: {item.rain_probability}%</Text>
+                            </View>
+                        )}
+                    />
+                </ScrollView>
+            </View>
         </View>
     )
 }
@@ -52,5 +98,59 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
+        padding: 10,
+        gap: 20,
+
+    },
+    card: {
+        width: 80,
+        height: 80,
+        backgroundColor: colors.componentBackground,
+        borderRadius: 18,
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: 10
+    },
+    row: {
+        flexDirection: 'row',
+        gap: 10
+    },
+    box: {
+        justifyContent: 'center',
+        alignItems: 'center',
+
+    },
+    title: {
+        color: colors.textColor,
+        fontSize: 30,
+    },
+    locationText: {
+        color: colors.textColor,
+        fontSize: 18
+    },
+    text: {
+        color: colors.textColor,
+        fontSize: 18
+    },
+    scrollView: {
+        gap: 10,
+        width: '100%',
+        height: 230,
+        borderRadius: 15
+    },
+    hourlyCard: {
+        backgroundColor: colors.componentBackground,
+        padding: 10,
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        width: 110,
+        height: '100%',
+        borderRadius: 10,
+        marginRight: 10,
+        gap: 5
+    },
+    textCard: {
+        color: colors.textColor,
+        fontSize: 14
     },
 })
